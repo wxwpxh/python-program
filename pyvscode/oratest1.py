91d8c6d486a76128aa261cb282324bd3a8e81bf4
wxwpxh <15305732366@189.cn>
2021-08-14 14:40:53 +0800

    update

--- pyvscode/oratest1.py
+++ pyvscode/oratest1.py
@@ -5,7 +5,7 @@ conn= cx_Oracle.connect('mymotif/wxwpxh@orcl')
 cur = conn.cursor() 			
 cur.execute("select * from STUDENT")
 info = cur.fetchall() 
-print len(info)   	#获得表中有多少条数据
+print(len(info))   	#获得表中有多少条数据
 for ii in info:
     print(ii[0]+' '+ii[1]+' '+ii[2]+' '+ii[3].strftime('%Y-%m-%d')+' '+ii[4])	
 cur.close()
